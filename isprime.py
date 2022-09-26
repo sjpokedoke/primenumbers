f16b3643cf045e98f2bea0e87d824d43b1eeebd7
sjpokedoke <70572845+sjpokedoke@users.noreply.github.com>
2022-09-26 17:59:25 +0800

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,7 +1,9 @@
 n = int(input("Please enter a whole number: "))
 
 if n > 1:
-    for i in range(2, int(n/2)+1):
+    if n == 2:
+        print(n, "is a prime number")
+    for i in range(2, int(n)):
         if (n%i == 0):
             print(n, "is not a prime number")
             break
